9109a1fe48f5b359c0f41f47fc0604e5212c200b
jgabriel1 <josegabriel@det.ufc.br>
2021-04-16 10:41:45 -0300

    Adapted Pagination component.

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,15 +1,21 @@
-import { HStack, Box } from '@chakra-ui/react';
+import { Stack, Box } from '@chakra-ui/react';
 
 import { PaginationItem } from './PaginationItem';
 
 export function Pagination() {
   return (
-    <HStack mt="8" justify="space-between" align="center" spacing="6">
+    <Stack
+      direction={['column', 'row']}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
       <Box>
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
 
-      <HStack spacing="2">
+      <Stack direction="row" spacing="2">
         <PaginationItem pageNumber={1} isCurrent />
 
         <PaginationItem pageNumber={2} />
@@ -17,7 +23,7 @@ export function Pagination() {
         <PaginationItem pageNumber={3} />
 
         <PaginationItem pageNumber={4} />
-      </HStack>
-    </HStack>
+      </Stack>
+    </Stack>
   );
 }
