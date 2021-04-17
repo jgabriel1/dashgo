c5a7bb0a54d38d83ec8254a38c2396e8ed262a7c
jgabriel1 <josegabriel@det.ufc.br>
2021-04-16 23:10:59 -0300

    Added query client provider to the app.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
+import { useRef } from 'react';
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../hooks/sidebarDrawer';
@@ -10,12 +12,16 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClient = useRef(new QueryClient());
+
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient.current}>
+      <ChakraProvider resetCSS theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 }
 
